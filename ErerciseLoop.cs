5b8298725be24819054a92608a4e5f0150d08cba
sudandhakall <138919040+sudandhakall@users.noreply.github.com>
2024-02-18 15:52:04 -0500

    Add files via upload

---
+++ ErerciseLoop.cs
@@ -0,0 +1,32 @@
+﻿//Author: Sudan Krishna Dhakal
+//Std:C0901145
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number;
+
+        // Repeat the procedure until a valid number is entered
+        do
+        {
+            Console.Write("Enter a positive number less than 10: ");
+            number = Convert.ToInt32(Console.ReadLine());
+        } while (number <= 0 || number >= 10);
+
+        Console.WriteLine($"Times table of number {number}:");
+
+        // Calculate and print the times table
+        for (int i = 1; i <= 9; i++)
+        {
+            int result = i * number;
+            Console.WriteLine($"{i} * {number} = {result}");
+        }
+
+        Console.WriteLine("---------");
+    }
+}
+
+
